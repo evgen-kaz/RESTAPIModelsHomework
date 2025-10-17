7997d8a7d6c9c84a6a409a2a8e1b6cfe7a751f07
evgen-kaz <141723080+evgen-kaz@users.noreply.github.com>
2025-10-17 13:19:34 +0800

    Update CustomAllureListener.java

--- src/test/java/helpers/CustomAllureListener.java
+++ src/test/java/helpers/CustomAllureListener.java
@@ -3,11 +3,11 @@ package helpers;
 import io.qameta.allure.restassured.AllureRestAssured;
 
 public class CustomAllureListener {
-    private static final AllureRestAssured FILTER = new AllureRestAssured(); //взяли AllureRestAssured
+    private static final AllureRestAssured FILTER = new AllureRestAssured(); 
 
-    public static AllureRestAssured withCustomTemplates() { //и расширили его
-        FILTER.setRequestTemplate("request.ftl"); //добавили 1-й шаблон - запроса, что в папке resources
-        FILTER.setResponseTemplate("response.ftl"); //добавили 2-й шаблон - ответа, что в папке resources
+    public static AllureRestAssured withCustomTemplates() { 
+        FILTER.setRequestTemplate("request.ftl");
+        FILTER.setResponseTemplate("response.ftl");
         return FILTER;
     }
 }
